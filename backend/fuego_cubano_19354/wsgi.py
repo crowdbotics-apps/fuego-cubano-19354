a52c2bdddb270f43e764b707e73c3d8b7e6fc389
Crowdbotics <team@crowdbotics.com>
2020-08-04 03:39:58 +0000

    Update models

--- backend/fuego_cubano_19354/wsgi.py
+++ backend/fuego_cubano_19354/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fuego_cubano_19354.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fuego_cubano_19354.settings")
 
 application = get_wsgi_application()
